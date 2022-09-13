36ca7c0eda5dd0ccddb6c4c61219e3015509c8c4
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-09-14 00:34:24 +0530

    Add pydantic schemas

---
+++ app/db/schemas.py
@@ -0,0 +1,43 @@
+from datetime import date
+from pydantic import BaseModel
+from types import Union
+
+
+class ImageBase(BaseModel):
+    title: str
+    caption: str
+    tag: list[str] = []
+    upload_date: date
+    likes: int
+    user_id: int
+
+
+class ImageCreate(ImageBase):
+    link: str
+
+
+class Image(ImageBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+    
+
+class UserBase(BaseModel):
+    name: str
+    email: str
+    bio: str
+    social_media: str
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    images: list[Image] = []
+
+    class Config:
+        orm_mode = True
