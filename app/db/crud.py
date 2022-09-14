0be4ba071c684568cd0c023b24de059a1052ceb5
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-09-14 12:33:53 +0530

    Add basic create read methods for user and image

---
+++ app/db/crud.py
@@ -0,0 +1,59 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+# user crud functions
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + 'notreallyhashed'
+    # shortcut code -> **user.dict()
+    db_user = models.User(
+        name = user.name,
+        email = user.email,
+        bio = user.bio,
+        username = user.username,
+        social_media = user.social_media,
+        hashed_password = fake_hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+# image crud functions
+def get_image_by_id(db: Session, image_id: int):
+    return db.query(models.Image).filter(models.Image.id == image_id).first()
+
+
+def get_images_by_title(db: Session, image_title: str, limit: int = 100):
+    return db.query(models.Image).filter(models.Image.title == image_title).limit(limit).all()
+
+
+def get_images(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Image).offset(skip).limit(limit).all()
+
+
+def create_image(db: Session, item: schemas.ImageCreate, user_id: int):
+    db_image = models.Image(**item.dict(), user_id = user_id)
+    db.add(db_image)
+    db.commit()
+    db.refresh(db_image)
+    return db_image
+    
