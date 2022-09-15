df2417926d070909bf5e1f7089a3dcd81eb3521d
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-09-15 21:29:12 +0530

    Add user post and get routes with get, create methods from db

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,22 +1,57 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from ..db.main import SessionLocal
+from ..db import crud, schemas
+
+# Database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 router = APIRouter(
     prefix="/user"
 )
 
-@router.get("/all")
-async def get_all_users():
-    return [
-        {"username": "Plaban", "email": "plaban.kr.mondal00@gmail.com"},
-        {"username": "Plaban", "email": "plaban.kr.mondal00@gmail.com"},
-        {"username": "Plaban", "email": "plaban.kr.mondal00@gmail.com"}
-    ]
+
+# Get Routes
+@router.get("/all", response_model=list[schemas.User])
+async def get_all_users(skip: int = 0, limit: int = 0, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
 
-@router.get("/{id}")
-async def get_user_by_id(id: int):
-    return [
-        {"id": id, "username": "Plaban", "email": "plaban.kr.mondal00@gmail.com"}
-    ]
+@router.get("/{user_id}", response_model=schemas.User)
+async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
-    
+
+@router.get("/username/{username}", response_model=schemas.User)
+async def get_user_by_username(username: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_username(db, username)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@router.get("/email/{email}", response_model=schemas.User)
+async def get_user_by_email(email: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+# Post Routes
+@router.post("/new", response_model=schemas.User)
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, details="Email already registered")
+    return crud.create_user(db=db, user=user)
