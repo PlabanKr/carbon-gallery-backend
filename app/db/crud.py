5976d52acd0e7c7f1bbdef8befd28d8f605af010
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-09-15 21:58:50 +0530

    Add method to search image by user_id

--- app/db/crud.py
+++ app/db/crud.py
@@ -42,6 +42,10 @@ def get_image_by_id(db: Session, image_id: int):
     return db.query(models.Image).filter(models.Image.id == image_id).first()
 
 
+def get_image_by_user_id(db: Session, user_id: int):
+    return db.query(models.Image).filter(models.Image.user_id == user_id).first()
+
+
 def get_images_by_title(db: Session, image_title: str, limit: int = 100):
     return db.query(models.Image).filter(models.Image.title == image_title).limit(limit).all()
 
